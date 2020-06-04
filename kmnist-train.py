c1815bba79a62c7e9ebb10abb7bfe319bd5f2e05
Henrik Skogstrom <henrik.skogstrom@gmail.com>
2020-06-04 14:51:26 +0300

    Update kmnist-train.py

--- kmnist-train.py
+++ kmnist-train.py
@@ -1,10 +1,8 @@
 import os
 import glob
-import umap
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
-import seaborn as sns
 import itertools
 from os import listdir, makedirs, getcwd, remove
 from os.path import isfile, join, abspath, exists, isdir, expanduser
