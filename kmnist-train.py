15b492e2780428989168cf1bd2c00e3682b28d64
Aarni Koskela <akx@iki.fi>
2020-06-08 15:30:54 +0300

    Clean up imports

--- kmnist-train.py
+++ kmnist-train.py
@@ -1,43 +1,20 @@
-import os
-import glob
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
-import itertools
-from os import listdir, makedirs, getcwd, remove
-from os.path import isfile, join, abspath, exists, isdir, expanduser
-from PIL import Image
 from pathlib import Path
-from mpl_toolkits.mplot3d import Axes3D
-from keras.models import Sequential, Model
-from keras.applications import vgg16
-from keras.applications import resnet50
-from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Input, Flatten
-from keras.layers import GlobalMaxPooling2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers.merge import Concatenate
-from keras.optimizers import Adam, RMSprop
-from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping, ReduceLROnPlateau
+
+import numpy as np  # linear algebra
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.models import Sequential
 from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.manifold import TSNE
-from skimage.io import imread, imshow
-from skimage.transform import resize
-from keras import backend as K
-import tensorflow as tf
-from collections import defaultdict, Counter
 
 # Let us define some paths first
 input_path = Path("/valohai/inputs/example-data")
 
 # Path to training images and corresponding labels provided as numpy arrays
-kmnist_train_images_path = input_path/"kmnist-train-imgs.npz"
-kmnist_train_labels_path = input_path/"kmnist-train-labels.npz"
+kmnist_train_images_path = input_path / "kmnist-train-imgs.npz"
+kmnist_train_labels_path = input_path / "kmnist-train-labels.npz"
 
 # Path to the test images and corresponding labels
-kmnist_test_images_path = input_path/"kmnist-test-imgs.npz"
-kmnist_test_labels_path = input_path/"kmnist-test-labels.npz"
+kmnist_test_images_path = input_path / "kmnist-test-imgs.npz"
+kmnist_test_labels_path = input_path / "kmnist-test-labels.npz"
 
 # Load the training data from the corresponding npz files
 kmnist_train_images = np.load(kmnist_train_images_path)['arr_0']
@@ -47,7 +24,8 @@ kmnist_train_labels = np.load(kmnist_train_labels_path)['arr_0']
 kmnist_test_images = np.load(kmnist_test_images_path)['arr_0']
 kmnist_test_labels = np.load(kmnist_test_labels_path)['arr_0']
 
-print(f"Number of training samples: {len(kmnist_train_images)} where each sample is of size: {kmnist_train_images.shape[1:]}")
+print(
+    f"Number of training samples: {len(kmnist_train_images)} where each sample is of size: {kmnist_train_images.shape[1:]}")
 print(f"Number of test samples: {len(kmnist_test_images)} where each sample is of size: {kmnist_test_images.shape[1:]}")
 
 # A bunch of variables. The variable have the same value as given in the keras example
@@ -102,4 +80,4 @@ model.save("/valohai/outputs/model.h5")
 # Check the test loss and test accuracy 
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
-print('Test accuracy:', score[1]*100)
+print('Test accuracy:', score[1] * 100)
